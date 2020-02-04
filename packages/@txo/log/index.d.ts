cd57519ae4a51402e2497bde647866831b9270f5
Erik Slovak <erik.slovak@technologystudio.sk>
2020-02-04 17:52:29 +0100

    update index.d.ts files

--- packages/@txo/log/index.d.ts
+++ packages/@txo/log/index.d.ts
@@ -1,2 +1,17 @@
 declare module "@txo/log" {
+  class Log {
+    constructor(namespace: string, level?: Level);
+    error (message: string, payload?: any, options?: Options): void;
+    warning (message: string, payload?: any, options?: Options): void;
+    info(message: string, payload?: any, options?: Options): void;
+    debug (message: string, payload?: any, options?: Options): void;
+    errorProxy <RESULT> (message: string, result: RESULT, options?: Options): RESULT;
+    warningProxy <RESULT> (message: string, result: RESULT, options?: Options): RESULT;
+    infoProxy <RESULT> (message: string, result: RESULT, options?: Options): RESULT
+    debugProxy <RESULT> (message: string, result: RESULT, options?: Options): RESULT;
+    errorLazy (message: string, payloadCallback: () => any, options?: Options): void;
+    warningLazy (message: string, payloadCallback: () => any, options?: Options): void;
+    infoLazy (message: string, payloadCallback: () => any, options?: Options): void;
+    debugLazy (message: string, payloadCallback: () => any, options?: Options): void;
+  }
 }
